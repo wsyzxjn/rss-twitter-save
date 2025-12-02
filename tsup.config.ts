3aa41ba7cef37379bdee1dab1dc322e1800acce4
Amatsuka <45129539+wsyzxjn@users.noreply.github.com>
2025-12-03 04:26:27 +0800

    移除生产环境压缩配置
    - 删除 NODE_ENV 环境变量判断逻辑
    - 移除 minify 配置项,不再根据环境进行代码压缩

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,9 @@
 // tsup.config.ts
 import { defineConfig, type Options } from "tsup";
 
-const isProduction = process.env.NODE_ENV === "production";
-
 const DEFAULT_CONFIG: Options = {
   entry: ["src/index.ts"],
   dts: true,
-  minify: isProduction,
   clean: true,
   outDir: "dist",
   skipNodeModulesBundle: true,
